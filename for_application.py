93f519c311c2e6baa5a84a6026ac506774f03a5c
Shengli Jiang <sjiang87@wisc.edu>
2020-01-02 18:53:01 -0600

    stock return

--- for_application.py
+++ for_application.py
@@ -1,131 +1,111 @@
 # import modules
 import numpy as np
-import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.metrics import r2_score, mean_squared_error
-from sklearn.model_selection import KFold
 import matplotlib.pyplot as plt
 
-def generate_data():
-    '''
-    A function to read the stock data and output standardized x and y
-    args: None
-    return: standardized x, y and column names
-    '''
-    # import feature and returns
-    df = pd.read_csv(r'../yujia_data/data/project_machine.csv')
-    column_names_old = list(df)
-    df = df[df['sic2']==54]
-    df = df.fillna(0)
-    #df = df.dropna(axis=1)
-    not_include = ['RET','DATE', 'permno', 'sic2', 'gvkey', 'fyear', 'datadate',
-                   'rdq', 'ewret', 'DLRET', 'DLSTCD']
-    x = df.drop(not_include, axis=1)
-    column_names = [e for e in column_names_old if e not in not_include] 
-    y = df['RET']
-    
-    # standardize the data
-    
-    min_max_scaler = MinMaxScaler()
-    x = min_max_scaler.fit_transform(x)
-    y = y.values
+# import my own mudules
+from utils import generate_data, lasso, ridge, ols, k_fold, bayesianridge, svr, sgdregressor, decisiontree
+from plot_tools import plot_pred
 
-    return x, y, column_names
+x, y, column_names = generate_data(sic2_number=1)
 
-def plot_pred(y_test, y_pred, title):
-    '''
-    A function to plot the actual and predicted stock return
-    args: actual return, predicted return, type of regression
-    return: save an image
-    '''
-    plt.figure()
-    plt.scatter(y_test, y_pred)
-    plt.xlabel('Actual return')
-    plt.ylabel('Predicted return')
-    plt.grid()
-    plt.savefig('./plots/{}.svg'.format(title), dpi=400)
-    plt.close()
 
-def lasso(x_train, x_test, y_train, y_test, alpha=0.0001):
-    '''
-    A lasso regression function:
-        
-    '''
-    clf = Lasso(alpha=alpha, max_iter=10000)
-    clf.fit(x_train, y_train)
-    y_pred = clf.predict(x_test)
-    coef = clf.coef_.squeeze()
-    return y_pred, coef#[column_names[i] for i in coef_max]
 
-def ridge(x_train, x_test, y_train, y_test):
-    clf = Ridge(alpha=1, max_iter=10000)
-    clf.fit(x_train, y_train)
-    y_pred = clf.predict(x_test)
-    coef = clf.coef_.squeeze()
-    coef_max = np.abs(coef).argsort()[-25:][::-1]
-    return y_pred, [column_names[i] for i in coef_max]
+# Ordinary Least Square
+def evaluation(x, y, metric, alpha):
+    x_trains, x_tests, y_trains, y_tests = k_fold(x, y, n_splits=4)
+    y_true = []
+    y_pred = []
+    for i in range(len(x_trains)):
+        x_train = x_trains[i]
+        x_test = x_tests[i]
+        y_train = y_trains[i]
+        y_test = y_tests[i]
+        y_pred.append(metric(x_train, x_test, y_train, y_test, alpha)[0])
+        y_true.append(y_tests[i])
+    y_true = np.concatenate(y_true)
+    y_pred = np.concatenate(y_pred)
+    # evaluation
+    plot_pred(y_true, y_pred, metric.__name__)
+    r2 = r2_score(y_true, y_pred)
+    rmse = mean_squared_error(y_true, y_pred)**0.5
+    print('R2: {:.4f}'.format(r2))
+    print('RMSE: {:.4f}'.format(rmse))
+    return r2, rmse
 
-def ols(x_train, x_test, y_train, y_test):
-    clf = LinearRegression()
-    clf.fit(x_train, y_train)
-    y_pred = clf.predict(x_test)
-    coef = clf.coef_.squeeze()
-    coef_max = np.abs(coef).argsort()[-25:][::-1]
-    return y_pred, [column_names[i] for i in coef_max]
+evaluation(x, y, metric=ols, alpha=0)
+evaluation(x, y, metric=ridge, alpha=0.2)
+evaluation(x, y, metric=lasso, alpha=0.0001)
+evaluation(x, y, metric=bayesianridge, alpha=0.0001)
+evaluation(x, y, metric=svr, alpha=0)
+evaluation(x, y, metric=sgdregressor, alpha=0)
+evaluation(x, y, metric=decisiontree, alpha=0)
 
-x, y, column_names = generate_data()
 
-# 4 fold validation
-kf = KFold(n_splits=4, shuffle=True, random_state=42)
-kf.get_n_splits(x)
-y_tests, y_preds, coefs = [], [], []
-for train_index, test_index in kf.split(x):
-    x_train, x_test = x[train_index], x[test_index]
-    y_train, y_test = y[train_index], y[test_index]
-    y_pred, coef = lasso(x_train, x_test, y_train, y_test)
-    y_tests.append(y_test)
-    y_preds.append(y_pred)
-    coefs.append(coef)
-y_test = np.concatenate(y_tests)
-y_pred = np.concatenate(y_preds)
-coef = np.abs(np.array(coefs).sum(axis=0))
-coef_max = np.abs(coef).argsort()[-50:][::-1]
+# select descriptors
+# Removing features with low variance
+from sklearn.feature_selection import VarianceThreshold
+sel = VarianceThreshold(.99 * (1 - .99))
+x_vt = sel.fit_transform(x)
+evaluation(x_vt, y, metric=ols, alpha=0)
+evaluation(x_vt, y, metric=lasso, alpha=0.0001)
+
+# Univariate feature selection
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LinearRegression, Lasso, Ridge, BayesianRidge
+lr = LinearRegression()
+rfe = RFE(estimator=lr, n_features_to_select=90, step=1)
+rfe.fit(x, y)
+ranking = rfe.ranking_
+select = rfe.support_
+x_rfe = x[:, select]
+evaluation(x_rfe, y, metric=ols, alpha=0)
+#%%
+
+
+#
+#coef = np.abs(np.array(coefs).sum(axis=0))
+#coef_max = np.abs(coef).argsort()[-50:][::-1]
 
 
 #%%
-x_train = x_train[:, coef_max]
-x_test = x_test[:, coef_max]
+#x_train = x_train[:, coef_max]
+#x_test = x_test[:, coef_max]
 #%%
-import keras
-from keras import Sequential
-from keras.layers import Dense, BatchNormalization, Dropout
-from keras.callbacks import ModelCheckpoint, EarlyStopping
-np.random.seed(42)
-def regression_cnn():
-    model = Sequential()
-    model.add(Dense(256, activation='relu', input_shape=(50,)))
-    model.add(Dropout(0.5))
-    model.add(Dense(256, activation = 'relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(1, activation = 'linear'))
-    return model
-
-weight_name = r'./yujia.h5'
-model = regression_cnn()  
-adam = keras.optimizers.Adam(lr=0.01)
-model.compile(loss='mean_squared_error', optimizer= adam, metrics=['mean_squared_error'])
-elstp = EarlyStopping(monitor='val_loss', min_delta=0, patience=200, verbose=0, mode='min')
-checkpoint = ModelCheckpoint(weight_name, monitor='val_loss', verbose=0, save_best_only=True, mode='min')
-callbacks_list = [checkpoint,elstp]
-history = model.fit(x_train, y_train, epochs=2000, batch_size=len(y_train), validation_split=0.25, callbacks=callbacks_list, verbose=2)
-history_history = history.history
-model.load_weights(weight_name)
-model.compile(loss='mean_squared_error', optimizer= adam, metrics=['mean_squared_error'])
-y_pred = model.predict(x_test)
-print('Lasso R2: {}'.format(r2_score(y_test, y_pred)))
-print('Lasso RMSE: {}'.format(mean_squared_error(y_test, y_pred)**0.5))
-
+#import keras
+#from keras import Sequential
+#from keras.layers import Dense, BatchNormalization, Dropout
+#from keras.callbacks import ModelCheckpoint, EarlyStopping
+#np.random.seed(42)
+#def regression_cnn():
+#    model = Sequential()
+#    model.add(Dense(256, activation='relu', input_shape=(139,)))
+#    model.add(BatchNormalization())
+#    model.add(Dense(256, activation = 'relu'))
+#    model.add(BatchNormalization())
+#    model.add(Dense(1, activation = 'linear'))
+#    return model
+#
+#weight_name = r'./yujia.h5'
+#model = regression_cnn()  
+#adam = keras.optimizers.Adam(lr=0.001)
+#model.compile(loss='mean_squared_error', optimizer= adam, metrics=['mean_squared_error'])
+#elstp = EarlyStopping(monitor='val_loss', min_delta=0, patience=1000, verbose=0, mode='min')
+#checkpoint = ModelCheckpoint(weight_name, monitor='val_loss', verbose=0, save_best_only=True, mode='min')
+#callbacks_list = [checkpoint,elstp]
+#history = model.fit(x_train, y_train, epochs=20000, batch_size=len(y_train), validation_split=0.25, callbacks=callbacks_list, verbose=2)
+#history_history = history.history
+#model.load_weights(weight_name)
+#model.compile(loss='mean_squared_error', optimizer= adam, metrics=['mean_squared_error'])
+#y_pred = model.predict(x_test)
+#print('Lasso R2: {}'.format(r2_score(y_test, y_pred)))
+#print('Lasso RMSE: {}'.format(mean_squared_error(y_test, y_pred)**0.5))
+##%%
+#plt.scatter(y_test, y_pred)
+##%%
+#for key in history_history.keys():
+#    plt.plot(history_history[key], label=key)
+#    plt.legend()
 
     
 
---
+++ plot_tools.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 29 14:28:42 2019
+
+@author: Yujia Ren
+"""
+import matplotlib.pyplot as plt
+
+def plot_pred(y_test, y_pred, title):
+    '''
+    A function to plot the actual and predicted stock return
+    args: actual return, predicted return, type of regression
+    return: save an image
+    '''
+    plt.figure()
+    plt.scatter(y_test, y_pred)
+    plt.xlabel('Actual return')
+    plt.ylabel('Predicted return')
+    plt.grid()
+    plt.savefig('./plots/{}.svg'.format(title), dpi=400)
+    plt.close()
